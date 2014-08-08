f6eccc1e41d26013f96e6a3132a000037cd37d0d
oredi <darren_seet@oredi.com>
2014-08-09 03:55:05 +0900

    Added data importing

--- chapter5/blog-express/Gruntfile.js
+++ chapter5/blog-express/Gruntfile.js
@@ -7,26 +7,28 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         mochacliConfig: grunt.file.readJSON("mochacli-config.json"),
+        pathConfig: grunt.file.readJSON("path-config.json"),
 
         // Configure grunt-contrib-clean task
-        clean: ["dist"],
+        clean: ["<%= pathConfig.dist %>/"],
 
         // Configure grunt-contrib-jshint task
         jshint: {
             options: {
                 laxcomma: true
             },
-            all: ['Gruntfile.js', 'specs/**/*.js', 'dist/js/**/.js']
+            all: ['Gruntfile.js', '<%= pathConfig.specs %>/**/*.js', '<%= pathConfig.distJs %>/**/.js']
         },
 
         // Configure grunt-contrib-copy task
         copy: {
             main: {
                 files: [
-                    {cwd: 'src', expand: true, src: ['**/*.jade'], dest: 'dist/', filter: 'isFile'},
-                    {cwd: 'src/scripts', expand: true, src: ['**/*.js'], dest: 'dist/js/', filter: 'isFile'},
-                    {cwd: 'node_modules/jquery/dist/', expand: true, src: ['*.js'], dest: 'dist/lib/jquery/js', filter: 'isFile'},
-                    {cwd: 'node_modules/bootstrap/dist/', expand: true, src: ['**/*'], dest: 'dist/lib/bootstrap', filter: 'isFile'}
+                    {cwd: '<%= pathConfig.src %>', expand: true, src: ['**/*.jade'], dest: '<%= pathConfig.dist %>', filter: 'isFile'},
+                    {cwd: '<%= pathConfig.srcScripts %>', expand: true, src: ['**/*.js'], dest: '<%= pathConfig.distJs %>', filter: 'isFile'},
+                    {cwd: '<%= pathConfig.nodeModules %>/jquery/dist/', expand: true, src: ['*.js'], dest: '<%= pathConfig.distLib %>/jquery/js', filter: 'isFile'},
+                    {cwd: '<%= pathConfig.nodeModules %>/bootstrap/dist/', expand: true, src: ['**/*'], dest: '<%= pathConfig.distLib %>/bootstrap', filter: 'isFile'},
+                    {cwd: '<%= pathConfig.srcDataCollection %>', expand: true, src: ['**/*.json'], dest: '<%= pathConfig.distDataCollection %>', filter: 'isFile'}
                 ]
             }
         },
@@ -39,14 +41,39 @@ module.exports = function (grunt) {
                     ui: '<%= mochacliConfig.ui %>',
                     colors: '<%= mochacliConfig.colors %>'
                 },
-                src: ['specs/**/*.js']
+                src: ['<%= pathConfig.specs %>/**/*.js']
+            }
+        },
+
+        // Configure grunt-mongoimport
+        mongoimport : {
+            options: {
+                db: 'blog',
+                host: 'localhost',
+                port: '27017',
+                stopOnError: false,
+                collections: [
+                    {
+                        name: 'users',
+                        type: 'json',
+                        file: '<%= pathConfig.distDataCollection %>/users.json',
+                        jsonArray : true,
+                        upsert : true,
+                        drop : true
+                    },
+                    {
+                        name: 'articles',
+                        type: 'json',
+                        file: '<%= pathConfig.distDataCollection %>/articles.json',
+                        jsonArray : true,
+                        upsert : true,
+                        drop : true
+                    }
+                ]
             }
         }
-
-
-
     });
 
     // Define the default task
-    grunt.registerTask('default', ['clean', 'copy', 'jshint', 'mochacli']);
+    grunt.registerTask('default', ['clean', 'copy', 'jshint', 'mochacli', 'mongoimport']);
 };
